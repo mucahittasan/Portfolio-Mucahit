ac10ae34ae96ac865ba810b755aea76462afd522
mucahittasan <mucahittasan0@gmail.com>
2023-05-11 14:33:28 +0300

    Vercel.json file created

--- src/actions/getBlogDetail.ts
+++ src/actions/getBlogDetail.ts
@@ -3,7 +3,7 @@ import matter from 'gray-matter';
 
 export default function getBlogDetail(slug: string) {
 
-    const folder = "public/posts/";
+    const folder = "/src/posts/";
     const file = `${folder}${slug}.md`;
     const content = fs.readFileSync(file, "utf-8");
     const matterResult = matter(content);
